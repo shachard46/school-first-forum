d8e7609e3df47a8b902e83a5400e44cc2adde629
Shachar David <shachadavid2013@gmail.com>
2019-05-30 14:45:59 +0300

    fixed wrong period

--- src/main/java/com/shachar/first/Utils.java
+++ src/main/java/com/shachar/first/Utils.java
@@ -30,15 +30,22 @@ public class Utils {
 
 	public static String getPeriod(Date date) {
 		Period period = new Period(date.getTime(), (new Date()).getTime());
-		String diff = String.format("%d ימים", Math.abs(period.getDays()));
-		if (Math.abs(period.getDays()) == 0) {
+		System.out.println(period.getYears());
+		String diff;
+		if (Math.abs(period.getYears()) != 0) {
+			diff = String.format("%d שנים", Math.abs(period.getYears()));
+		} else if (Math.abs(period.getMonths()) != 0) {
+			diff = String.format("%d חודשים", Math.abs(period.getMonths()));
+		} else if(Math.abs(period.getWeeks()) != 0) {
+			diff = String.format("%d שבועות", Math.abs(period.getWeeks()));
+		} else if(Math.abs(period.getDays()) != 0) {
+			diff = String.format("%d ימים", Math.abs(period.getDays()));
+		} else if (Math.abs(period.getHours()) != 0) {
 			diff = String.format("%d שעות", Math.abs(period.getHours()));
-			if (Math.abs(period.getHours()) == 0) {
-				diff = String.format("%d דקות", Math.abs(period.getMinutes()));
-				if (Math.abs(period.getMinutes()) == 0) {
-					diff = "לפני פחות מדקה";
-				}
-			}
+		} else if(Math.abs(period.getMinutes()) != 0) {
+			diff = String.format("%d דקות", Math.abs(period.getMinutes()));
+		} else {
+			diff = "לפני פחות מדקה";
 		}
 		return diff;
 	}
@@ -53,15 +60,17 @@ public class Utils {
 		return formatter.format(date);
 	}
 
-	public static void main(String args[]) {
-		Calendar calendar = Calendar.getInstance();
-		calendar.set(2019, 4, 7, 12, 36, 14);
-		Date date = new Date();
-		date.setTime(calendar.getTimeInMillis());
+	public static String formatDateForRegister(Date date) {
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		return formatter.format(date);
 	}
-	
+
+	public static void main(String args[]) {
+		getPeriod(DatabaseManager.get().getUserDatabase().getAllEntities().get(0).getLastSeen());
+	}
+
 	public static boolean isEmptyOrNull(String s) {
-		return s == null || s.isEmpty(); 
+		return s == null || s.isEmpty();
 	}
 
 }
